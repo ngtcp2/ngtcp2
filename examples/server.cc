6abaa36df74d64712862bdecbee952f2899dea9f
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2019-04-04 09:30:14 +0900

    More placeholders

--- examples/server.cc
+++ examples/server.cc
@@ -1384,7 +1384,7 @@ int Handler::setup_httpconn() {
   nghttp3_conn_settings_default(&settings);
   settings.qpack_max_table_capacity = 4096;
   settings.qpack_blocked_streams = 100;
-  settings.num_placeholders = 10;
+  settings.num_placeholders = 100;
 
   auto mem = nghttp3_mem_default();
 
