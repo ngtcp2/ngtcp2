423185452ba3aa86c6b7d02e22481dbbfa58c9fb
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2020-12-09 10:34:51 +0900

    Refactor

--- lib/ngtcp2_conn.c
+++ lib/ngtcp2_conn.c
@@ -5695,17 +5695,16 @@ static int conn_recv_crypto(ngtcp2_conn *conn, ngtcp2_crypto_level crypto_level,
     if (rv != 0) {
       return rv;
     }
-  } else if (fr_end_offset - rx_offset > NGTCP2_MAX_REORDERED_CRYPTO_DATA) {
-    return NGTCP2_ERR_CRYPTO_BUFFER_EXCEEDED;
-  } else {
-    rv = ngtcp2_strm_recv_reordering(crypto, fr->data[0].base, fr->data[0].len,
-                                     fr->offset);
-    if (rv != 0) {
-      return rv;
-    }
+
+    return 0;
   }
 
-  return 0;
+  if (fr_end_offset - rx_offset > NGTCP2_MAX_REORDERED_CRYPTO_DATA) {
+    return NGTCP2_ERR_CRYPTO_BUFFER_EXCEEDED;
+  }
+
+  return ngtcp2_strm_recv_reordering(crypto, fr->data[0].base, fr->data[0].len,
+                                     fr->offset);
 }
 
 /*
