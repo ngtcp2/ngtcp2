e2bc77b4b23cea8433c2e425e61554b20de35c3b
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2021-01-17 17:01:13 +0900

    Build nghttp3 with ASAN enabled

--- ci/build_nghttp3.sh
+++ ci/build_nghttp3.sh
@@ -5,7 +5,9 @@ cd ..
 git clone https://github.com/ngtcp2/nghttp3
 cd nghttp3
 autoreconf -i
-./configure --prefix=$PWD/build --enable-lib-only
+./configure --prefix=$PWD/build --enable-lib-only \
+	    LDFLAGS="-fsanitize=address,undefined -fno-sanitize-recover=undefined" \
+	    CPPFLAGS="-fsanitize=address,undefined -fno-sanitize-recover=undefined -g3"
 make -j$(nproc) check
 make install
 cd ..
