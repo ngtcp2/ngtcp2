9c41c641a76670a7a4157f93376ccd3a1ebf1b87
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2021-01-17 17:19:31 +0900

    Revert "Build nghttp3 with ASAN enabled"
    This reverts commit e2bc77b4b23cea8433c2e425e61554b20de35c3b.

--- ci/build_nghttp3.sh
+++ ci/build_nghttp3.sh
@@ -5,9 +5,7 @@ cd ..
 git clone https://github.com/ngtcp2/nghttp3
 cd nghttp3
 autoreconf -i
-./configure --prefix=$PWD/build --enable-lib-only \
-	    LDFLAGS="-fsanitize=address,undefined -fno-sanitize-recover=undefined" \
-	    CPPFLAGS="-fsanitize=address,undefined -fno-sanitize-recover=undefined -g3"
+./configure --prefix=$PWD/build --enable-lib-only
 make -j$(nproc) check
 make install
 cd ..
