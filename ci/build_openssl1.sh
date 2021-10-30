db6508f5f67af3e91ca6458ec9eb9b16cb59213f
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2021-10-30 17:13:49 +0900

    Workaround broken quictls commit

--- ci/build_openssl1.sh
+++ ci/build_openssl1.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -e
 # build patched openssl (for GitHub workflow)
 
-git clone --depth 1 -b OpenSSL_1_1_1l+quic https://github.com/quictls/openssl
+git clone -b OpenSSL_1_1_1l+quic https://github.com/quictls/openssl
 cd openssl
+git checkout 5b312bf1bd1361216a817f338eca3830b7c15d85
 ./config --prefix=$PWD/build
 make -j$(nproc)
 make install_sw
