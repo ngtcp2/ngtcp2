b248f0ead41cf92a7a50fc67f85d1c36a8911df4
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2021-11-03 21:58:35 +0900

    Revert "Workaround broken quictls commit"
    This reverts commit db6508f5f67af3e91ca6458ec9eb9b16cb59213f.

--- ci/build_openssl3.sh
+++ ci/build_openssl3.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -e
 # build patched openssl (for GitHub workflow)
 
-git clone -b openssl-3.0.0+quic https://github.com/quictls/openssl
+git clone --depth 1 -b openssl-3.0.0+quic https://github.com/quictls/openssl
 cd openssl
-git checkout e034c2d95e31a383db94ea626c41d42a2b074f18
 ./config --prefix=$PWD/build
 make -j$(nproc)
 make install_sw
