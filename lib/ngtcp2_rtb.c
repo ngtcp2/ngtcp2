552b140674ae3ec99f63870df07d9b446e500595
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2021-11-06 11:27:58 +0900

    Check conn is not null

--- lib/ngtcp2_rtb.c
+++ lib/ngtcp2_rtb.c
@@ -840,7 +840,7 @@ ngtcp2_ssize ngtcp2_rtb_recv_ack(ngtcp2_rtb *rtb, const ngtcp2_ack *fr,
   /* Assume that ngtcp2_pkt_validate_ack(fr) returns 0 */
   it = ngtcp2_ksl_lower_bound(&rtb->ents, &largest_ack);
   if (ngtcp2_ksl_it_end(&it)) {
-    if (verify_ecn) {
+    if (conn && verify_ecn) {
       conn_verify_ecn(conn, pktns, rtb->cc, cstat, fr, ecn_acked,
                       largest_acked_sent_ts, ts);
     }
