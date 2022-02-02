d1fd895e50701e625746e07171fea0f5fd0c55ba
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2022-02-02 19:48:23 +0900

    h09server: Deal with EAGAIN/EWOULDBLOCK from sendmsg

--- examples/h09server.h
+++ examples/h09server.h
@@ -151,6 +151,13 @@ public:
   void singal_write();
   void add_sendq(Stream *stream);
 
+  void on_send_blocked(Endpoint &ep, const ngtcp2_addr &local_addr,
+                       const ngtcp2_addr &remote_addr, unsigned int ecn,
+                       const uint8_t *data, size_t datalen,
+                       size_t max_udp_payload_size);
+  void start_wev_endpoint(const Endpoint &ep);
+  int send_blocked_packet();
+
 private:
   struct ev_loop *loop_;
   Server *server_;
@@ -169,6 +176,23 @@ private:
   size_t nkey_update_;
   // draining_ becomes true when draining period starts.
   bool draining_;
+
+  struct {
+    bool send_blocked;
+    size_t num_blocked;
+    size_t num_blocked_sent;
+    // blocked field is effective only when send_blocked is true.
+    struct {
+      Endpoint *endpoint;
+      Address local_addr;
+      Address remote_addr;
+      unsigned int ecn;
+      const uint8_t *data;
+      size_t datalen;
+      size_t max_udp_payload_size;
+    } blocked[2];
+    std::unique_ptr<uint8_t[]> data;
+  } tx_;
 };
 
 class Server {
