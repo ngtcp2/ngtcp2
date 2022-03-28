cca79a2e186702c0e9ddca8523c06dc9c46795f2
Tatsuhiro Tsujikawa <404610+tatsuhiro-t@users.noreply.github.com>
2022-03-29 08:38:07 +0900

    Fix format specifier

--- lib/ngtcp2_conn.c
+++ lib/ngtcp2_conn.c
@@ -6132,7 +6132,7 @@ conn_recv_handshake_pkt(ngtcp2_conn *conn, const ngtcp2_path *path,
         ngtcp2_log_info(
             &conn->log, NGTCP2_LOG_EVENT_PKT,
             "Initial packet was ignored because it is included in UDP datagram "
-            "less than %zu bytes: %s bytes",
+            "less than %zu bytes: %zu bytes",
             NGTCP2_MAX_UDP_PAYLOAD_SIZE, dgramlen);
         return NGTCP2_ERR_DISCARD_PKT;
       }
