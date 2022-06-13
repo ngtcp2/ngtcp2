91317f7810f4c886bea649b7f7029fef87837652
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2022-06-13 23:39:12 +0900

    clang-format

--- examples/tls_client_context_openssl.cc
+++ examples/tls_client_context_openssl.cc
@@ -111,8 +111,8 @@ int TLSClientContext::init(const char *private_key_file,
   }
 
   if (config.session_file) {
-    SSL_CTX_set_session_cache_mode(
-        ssl_ctx_, SSL_SESS_CACHE_CLIENT | SSL_SESS_CACHE_NO_INTERNAL);
+    SSL_CTX_set_session_cache_mode(ssl_ctx_, SSL_SESS_CACHE_CLIENT |
+                                                 SSL_SESS_CACHE_NO_INTERNAL);
     SSL_CTX_sess_set_new_cb(ssl_ctx_, new_session_cb);
   }
 
