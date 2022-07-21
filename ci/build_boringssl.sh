69679a38826aabd625bee9c019cc123a0bc0e6c7
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2022-07-21 17:52:15 +0900

    Bump boringssl

--- ci/build_boringssl.sh
+++ ci/build_boringssl.sh
@@ -3,7 +3,7 @@
 
 git clone https://boringssl.googlesource.com/boringssl
 cd boringssl
-git checkout 27ffcc6e19bbafddf1b59ec0bc6df2904de7eb2c
+git checkout b95124305ab15c7523d3e21437309fa5dd717ee8
 mkdir build
 cd build
 cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
