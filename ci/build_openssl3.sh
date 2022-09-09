8b03a3feb2b3e3106d41e6ef1a63f45948b7bc27
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2022-09-09 21:25:50 +0900

    workflow: Run examples/tests
    Specify --openssldir to the system path in order to avoid
    'unregistered scheme' error when receiving client certificate, which
    is a yet another regression introduced in OpenSSL v3.

--- ci/build_openssl3.sh
+++ ci/build_openssl3.sh
@@ -3,6 +3,6 @@
 
 git clone --depth 1 -b openssl-3.0.5+quic https://github.com/quictls/openssl
 cd openssl
-./config --prefix=$PWD/build
+./config --prefix=$PWD/build --openssldir=/etc/ssl
 make -j"$(nproc 2> /dev/null || sysctl -n hw.ncpu)"
 make install_sw
