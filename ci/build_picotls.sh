7a929970cf8cf30bcdf7cfe237a860a25a1f654b
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2023-02-21 17:14:25 +0900

    Add workflow cache to speed up the builds

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -1,11 +1,26 @@
 #!/bin/sh -e
 # build picotls (for GitHub workflow)
 
-git clone https://github.com/h2o/picotls/
-cd picotls
-git checkout 9398c491cf5b21e123ce409f2d32a1d235d93b7e
+if [ "${OPENSSL}" = "openssl1" ]; then
+    WORKSPACE=picotls-openssl1
+else
+    WORKSPACE=picotls-openssl3
+fi
+
+git clone https://github.com/h2o/picotls/ "${WORKSPACE}"
+cd "${WORKSPACE}"
+git checkout "${PICOTLS_VERSION}"
 git submodule update --init
 mkdir build
 cd build
-PKG_CONFIG_PATH=$PWD/../../openssl/build/lib/pkgconfig:$PWD/../../openssl/build/lib64/pkgconfig cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
+if [ "${OPENSSL}" = "openssl1" ]; then
+    PKG_CONFIG_PATH=$PWD/../../openssl1/build/lib/pkgconfig
+else
+    PKG_CONFIG_PATH=$PWD/../../openssl3/build/lib/pkgconfig:$PWD/../../openssl3/build/lib64/pkgconfig
+fi
+
+export PKG_CONFIG_PATH
+
+cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
+
 make -j"$(nproc 2> /dev/null || sysctl -n hw.ncpu)"
