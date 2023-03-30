023296e05581a877b5cfbb8ecfd587838e813a01
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2023-03-30 18:17:15 +0900

    Amend c6f924715aad8844ab7a48dbf7ce55945b76f548

--- ci/build_nghttp3.sh
+++ ci/build_nghttp3.sh
@@ -3,6 +3,7 @@
 
 git clone https://github.com/ngtcp2/nghttp3
 cd nghttp3
+git checkout 2eda009319eceec3544d7a164b52be873a928ac0
 autoreconf -i
 ./configure --prefix=$PWD/build --enable-lib-only
 make -j"$(nproc 2> /dev/null || sysctl -n hw.ncpu)" check
