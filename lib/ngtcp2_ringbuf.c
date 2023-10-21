4d69e176e7ac84ee9c085a420cdd61cab688daf8
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2023-10-21 18:10:11 +0900

    Make custom __popcnt static

--- lib/ngtcp2_ringbuf.c
+++ lib/ngtcp2_ringbuf.c
@@ -33,7 +33,7 @@
 
 #if defined(_MSC_VER) && !defined(__clang__) &&                                \
     (defined(_M_ARM) || defined(_M_ARM64))
-unsigned int __popcnt(unsigned int x) {
+static unsigned int __popcnt(unsigned int x) {
   unsigned int c = 0;
   for (; x; ++c) {
     x &= x - 1;
