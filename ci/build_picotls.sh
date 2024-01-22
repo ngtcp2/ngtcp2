9ada8477c0b60285c409a25f401c4b2d894a1db5
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2024-01-22 17:27:11 +0900

    Use cmake -B consistently

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -14,16 +14,14 @@ git remote add origin https://github.com/h2o/picotls
 git fetch origin --depth 1 "${PICOTLS_VERSION}"
 git checkout "${PICOTLS_VERSION}"
 git submodule update --init --depth 1
-mkdir build
-cd build
 if [ "${OPENSSL}" = "openssl1" ]; then
-    PKG_CONFIG_PATH=$PWD/../../openssl1/build/lib/pkgconfig
+    PKG_CONFIG_PATH=$PWD/../openssl1/build/lib/pkgconfig
 else
-    PKG_CONFIG_PATH=$PWD/../../openssl3/build/lib/pkgconfig:$PWD/../../openssl3/build/lib64/pkgconfig
+    PKG_CONFIG_PATH=$PWD/../openssl3/build/lib/pkgconfig:$PWD/../openssl3/build/lib64/pkgconfig
 fi
 
 export PKG_CONFIG_PATH
 
-cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
+cmake -B build -DCMAKE_POSITION_INDEPENDENT_CODE=ON
 
-make -j"$(nproc 2> /dev/null || sysctl -n hw.ncpu)"
+make -j"$(nproc 2> /dev/null || sysctl -n hw.ncpu)" -C build
