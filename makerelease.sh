645b283ddc955932161194c9b3c929f994cb5e54
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2024-02-17 10:58:32 +0900

    makerelease.sh: Run git submodule update

--- makerelease.sh
+++ makerelease.sh
@@ -6,6 +6,8 @@ PREV_TAG=$2
 git checkout refs/tags/$TAG
 git log --pretty=fuller --date=short refs/tags/$PREV_TAG..HEAD > ChangeLog
 
+git submodule update --init --depth 1
+
 autoreconf -i
 ./configure && \
     make dist-bzip2 && make dist-gzip && make dist-xz || echo "error"
