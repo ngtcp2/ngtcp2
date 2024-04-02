ec2426c1732c69d343dbbdf3865a883ce99f6346
Tatsuhiro Tsujikawa <404610+tatsuhiro-t@users.noreply.github.com>
2024-04-02 18:29:23 +0900

    ci: Add macos-14
    This commit adds macos-14 build.  The existing macos-12 is replaced
    with macos-13.  Bump BoringSSL so that it can be built without Go.
    Change cache key to include runner name rather than OS name which is
    shared by macos runners, and different versions of Ubuntu when we
    upgrade it.

--- ci/build_wolfssl.sh
+++ ci/build_wolfssl.sh
@@ -5,6 +5,7 @@ git clone --depth 1 -b "${WOLFSSL_VERSION}" https://github.com/wolfSSL/wolfssl
 cd wolfssl
 autoreconf -i
 ./configure --prefix=$PWD/build --enable-all \
-            --enable-aesni --enable-harden --enable-keylog-export --disable-ech
+            --enable-harden --enable-keylog-export --disable-ech \
+            $EXTRA_CONFIGURE_FLAGS
 make -j"$(nproc 2> /dev/null || sysctl -n hw.ncpu)"
 make install
