3314622f6f940d2646c02a207e19a714c3953fb6
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2024-04-05 16:49:15 +0900

    server: Fix stateless reset buffer stack buffer overflow

--- examples/server.cc
+++ examples/server.cc
@@ -2918,7 +2918,7 @@ int Server::send_stateless_reset(size_t pktlen, const uint8_t *dcid,
 
   // SCID + minimum expansion - NGTCP2_STATELESS_RESET_TOKENLEN
   constexpr size_t max_rand_byteslen =
-      NGTCP2_SV_SCIDLEN + 22 - NGTCP2_STATELESS_RESET_TOKENLEN;
+      NGTCP2_MAX_CIDLEN + 22 - NGTCP2_STATELESS_RESET_TOKENLEN;
 
   size_t rand_byteslen;
 
