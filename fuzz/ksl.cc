1ca7af0e69524c68611adfdb5c7284223ed4f427
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2024-12-15 19:07:36 +0900

    Update ksl fuzzer with FuzzedDataProvider

--- fuzz/ksl.cc
+++ fuzz/ksl.cc
@@ -3,6 +3,8 @@
 #include <cstring>
 #include <memory>
 
+#include <fuzzer/FuzzedDataProvider.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif // defined(__cplusplus)
@@ -14,7 +16,7 @@ extern "C" {
 #endif // defined(__cplusplus)
 
 using KeyType = uint64_t;
-using DataType = int64_t;
+using DataType = uint64_t;
 
 namespace {
 int less(const ngtcp2_ksl_key *lhs, const ngtcp2_ksl_key *rhs) {
@@ -27,46 +29,48 @@ ngtcp2_ksl_search_def(less, less)
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   constexpr size_t keylen = sizeof(KeyType);
+  FuzzedDataProvider fuzzed_data_provider(data, size);
 
   ngtcp2_ksl ksl;
 
   ngtcp2_ksl_init(&ksl, less, ksl_less_search, keylen, ngtcp2_mem_default());
 
-  for (; size >= keylen; ++data, --size) {
-    KeyType d;
+  for (; fuzzed_data_provider.remaining_bytes();) {
+    auto add = fuzzed_data_provider.ConsumeBool();
+    auto key = fuzzed_data_provider.ConsumeIntegral<uint64_t>();
 
-    memcpy(&d, data, keylen);
+    if (add) {
+      auto data = std::make_unique<DataType>(key);
+      auto rv = ngtcp2_ksl_insert(&ksl, nullptr, &key, data.get());
+      if (rv == 0) {
+        data.release();
+      }
+    } else {
+      auto it = ngtcp2_ksl_lower_bound(&ksl, &key);
 
-    for (size_t i = 0; i < 2; ++i, d = bswap_64(d)) {
-      auto add = (d & 0x8000000000000000llu) != 0;
-      auto rm = (d & 0x4000000000000000llu) != 0;
-      auto key = static_cast<KeyType>(d & 0x7fffffffffffffffllu);
+      if (!ngtcp2_ksl_it_end(&it)) {
+        auto data = static_cast<DataType *>(ngtcp2_ksl_it_get(&it));
+        int rv;
 
-      if (add) {
-        auto data = std::make_unique<DataType>(key);
-        auto rv = ngtcp2_ksl_insert(&ksl, nullptr, &key, data.get());
+        if (*static_cast<KeyType *>(ngtcp2_ksl_it_key(&it)) == key) {
+          rv = ngtcp2_ksl_remove_hint(&ksl, nullptr, &it, &key);
+        } else {
+          rv = ngtcp2_ksl_remove(&ksl, nullptr, &key);
+        }
+
         if (rv == 0) {
-          data.release();
+          delete data;
         }
       }
-
-      auto it = ngtcp2_ksl_lower_bound(&ksl, &key);
-      if (ngtcp2_ksl_it_end(&it)) {
-        continue;
-      }
-
-      if (!rm) {
-        continue;
-      }
-
-      delete static_cast<DataType *>(ngtcp2_ksl_it_get(&it));
-
-      if (*static_cast<KeyType *>(ngtcp2_ksl_it_key(&it)) == key) {
-        ngtcp2_ksl_remove(&ksl, nullptr, &key);
-      } else {
-        ngtcp2_ksl_remove_hint(&ksl, nullptr, &it, &key);
-      }
     }
+
+    for (auto it = ngtcp2_ksl_begin(&ksl); !ngtcp2_ksl_it_end(&it);
+         ngtcp2_ksl_it_next(&it))
+      ;
+
+    for (auto it = ngtcp2_ksl_end(&ksl); !ngtcp2_ksl_it_begin(&it);
+         ngtcp2_ksl_it_prev(&it))
+      ;
   }
 
   for (auto it = ngtcp2_ksl_begin(&ksl); !ngtcp2_ksl_it_end(&it);
