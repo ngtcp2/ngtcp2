1335d7285f721d71654bad7ddd41dbbdf476c24e
Karthik Dasari <92445174+Karthikdasari0423@users.noreply.github.com>
2025-06-19 14:45:05 +0530

    Update client_base.h

--- examples/client_base.h
+++ examples/client_base.h
@@ -193,6 +193,7 @@ struct Config {
   std::vector<uint16_t> pmtud_probes;
   // ech_config_list contains ECHConfigList.
   std::vector<uint8_t> ech_config_list;
+  std::string client_ip;
 };
 
 class ClientBase {
