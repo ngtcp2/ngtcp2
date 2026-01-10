9c67137f26803cbe83cf4363c7ce9d708d4ed470
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2026-01-10 16:13:45 +0900

    examples: Add eventcb to Sim

--- examples/sim.cc
+++ examples/sim.cc
@@ -657,6 +657,12 @@ Event Channel::get_next_event() {
   };
 }
 
+void Channel::run_eventcb(Timestamp ts) {
+  if (link_config_.eventcb) {
+    link_config_.eventcb(ts, link_config_);
+  }
+}
+
 Simulator::Simulator(Endpoint client, Endpoint server)
   : client_{std::move(client)}, server_{std::move(server)} {}
 
@@ -726,6 +732,9 @@ int Simulator::run() {
 
     client_chan.pop_tx_queue();
     server_chan.pop_tx_queue();
+
+    client_chan.run_eventcb(ts);
+    server_chan.run_eventcb(ts);
 
     switch (event.type) {
     case EVENT_TYPE_TIMEOUT: {
