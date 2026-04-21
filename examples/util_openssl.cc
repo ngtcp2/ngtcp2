582d43c5d64ee334887438f598beeb0127dff351
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2026-04-21 18:53:27 +0900

    clang-format

--- examples/util_openssl.cc
+++ examples/util_openssl.cc
@@ -166,9 +166,11 @@ const char *crypto_default_ciphers() {
 
 const char *crypto_default_groups() {
   return "X25519:P-256:P-384:P-521"
-#if defined(WITH_EXAMPLE_BORINGSSL) || defined(WITH_EXAMPLE_OSSL) || defined(LIBRESSL_VERSION_NUMBER)
+#if defined(WITH_EXAMPLE_BORINGSSL) || defined(WITH_EXAMPLE_OSSL) ||           \
+  defined(LIBRESSL_VERSION_NUMBER)
          ":X25519MLKEM768"
-#endif // defined(WITH_EXAMPLE_BORINGSSL) || defined(WITH_EXAMPLE_OSSL)
+#endif // defined(WITH_EXAMPLE_BORINGSSL) || defined(WITH_EXAMPLE_OSSL) ||
+       // defined(LIBRESSL_VERSION_NUMBER)
     ;
 }
 
