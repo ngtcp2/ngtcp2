d0846c1735f9beeb0db2ac6004db035cb47dd470
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2026-09-10 19:16:25 +0900

    Bump clang-format to 21

--- lib/ngtcp2_unreachable.h
+++ lib/ngtcp2_unreachable.h
@@ -43,10 +43,10 @@
 #ifdef _MSC_VER
 __declspec(noreturn)
 #endif /* defined(_MSC_VER) */
-    void ngtcp2_unreachable_fail(const char *file, int line, const char *func)
+void ngtcp2_unreachable_fail(const char *file, int line, const char *func)
 #ifndef _MSC_VER
-        __attribute__((noreturn))
+  __attribute__((noreturn))
 #endif /* !defined(_MSC_VER) */
-        ;
+  ;
 
 #endif /* !defined(NGTCP2_UNREACHABLE_H) */
